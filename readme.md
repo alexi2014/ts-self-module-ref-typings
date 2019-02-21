19f7f111d8042da81d8798fba7531462bfbd41a0
alex_ivanenko <iva_alex@mail.ru>
2019-02-21 15:33:00 +0300

    First commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+To compile the example  
+```sh
+npx typescript@3.3.3 
+```
---
+++ test1.ts
@@ -0,0 +1,7 @@
+import * as selfModule from './test1';
+export class MyClass1 {
+    theProp = 'abc';
+}
+export var v1: MyClass1;
+export var v2 = new MyClass1();
+
